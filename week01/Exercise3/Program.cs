a0aa6c5c610899b961262262e2cefce92b634b6d
BrookLynn Herring <brooklynnlisaherring@gmail.com>
2025-11-02 10:37:12 -0800

    assignment Completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using Microsoft.VisualBasic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        //Console.Write("What is the magic number? ");
+        //int magicNumber = int.Parse(Console.ReadLine());
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine()); 
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower!");
+            }
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher!");
+            }
+        }    
+        if (guess == magicNumber)
+        {
+            Console.Write("Great job! You guessed the number"); 
+        }
+        else
+        {
+            Console.Write("Oops somthing went wrong. Try again! ");
+        }
     }
 }
