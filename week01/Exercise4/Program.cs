4dd61880324770b954348beb4785538edf8e552b
BrookLynn Herring <brooklynnlisaherring@gmail.com>
2025-11-03 13:52:34 -0800

    assignment complete

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Transactions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int userNumber = 9;
+        List<int> numbers = new List<int>();
+
+        //making the list
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number: ");
+            userNumber = int.Parse(Console.ReadLine());
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+        int total = 0;
+        int largest = numbers[0];
+        // finding total
+        foreach (int num in numbers)
+        {
+            total += num;
+        }
+        //amount of numbers in list
+        int count = numbers.Count;
+        // the average of the numbers
+        float average = total / count;
+        //finding the largest number
+        foreach (int num in numbers)
+        {
+            if (num > largest)
+            {
+                largest = num;
+            }
+        }
+        //printig results
+        Console.WriteLine($"the sum is: {total}");
+        Console.WriteLine($"the averge is: {average}");
+        Console.WriteLine($"the largest number is: {largest}");
     }
 }
